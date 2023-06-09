b626d0924fa80940b0760e2380338b2011cdbeba
Made Supardi <grymore@gmail.com>
2023-06-09 15:16:03 +0800

    add parent

--- dist/injector.js
+++ dist/injector.js
@@ -1,8 +1,8 @@
 // Get the current src URL of the iframe
-const srcUrl = window.document.getElementById("upbond-iframe");
+const srcUrl = window.parent.document.getElementById("upbond-iframe");
 if (srcUrl) {
   // Create a new URL object of the parent page
-  const parentUrl = new URL(window.document.location.href);
+  const parentUrl = new URL(window.parent.document.location.href);
 
   // Get the query parameter value from the parent URL
   const loggedIn = parentUrl.searchParams.get("loggedIn");
@@ -16,6 +16,6 @@ if (srcUrl) {
     const updatedSrcUrl = `${srcUrl.url}&loggedIn=${loggedIn}&rehydrate=${rehydrate}&verifier=${verifier}&selectedAddress=${selectedAddress}&state=${state}`;
 
     // Update the src attribute of the iframe with the new URL
-    // window.document.getElementById("upbond-iframe").src = updatedSrcUrl;
+    window.parent.document.getElementById("upbond-iframe").src = updatedSrcUrl;
   }
 }
