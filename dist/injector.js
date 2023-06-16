214c4e17f996bd1d35174429a5f601739cdfbc68
Made Supardi <grymore@gmail.com>
2023-06-16 17:43:04 +0800

    backup

--- dist/injector.js
+++ dist/injector.js
@@ -3,21 +3,31 @@ const srcUrl = window.parent.document.getElementById("upbond-iframe");
 if (srcUrl) {
   // Create a new URL object of the parent page
   const parentUrl = new URL(window.parent.document.location.href);
+  const backupUrl = localStorage.getItem("upbond_widget");
 
   // Get the query parameter value from the parent URL
-  const loggedIn = parentUrl.searchParams.get("loggedIn");
-  const rehydrate = parentUrl.searchParams.get("rehydrate");
-  const selectedAddress = parentUrl.searchParams.get("selectedAddress");
-  const state = parentUrl.searchParams.get("state");
-  const verifier = parentUrl.searchParams.get("verifier");
-
+  let loggedIn = parentUrl.searchParams.get("loggedIn");
+  let rehydrate = parentUrl.searchParams.get("rehydrate");
+  let selectedAddress = parentUrl.searchParams.get("selectedAddress");
+  let state = parentUrl.searchParams.get("state");
+  let verifier = parentUrl.searchParams.get("verifier");
+  if (backupUrl) {
+    const uri = JSON.parse(backupUrl);
+    if (uri) {
+      loggedIn = uri.loggedIn;
+      rehydrate = uri.rehydrate;
+      selectedAddress = uri.selectedAddress;
+      state = uri.state;
+      verifier = uri.verifier;
+    }
+  }
   if (loggedIn && verifier && rehydrate && selectedAddress && state) {
     // Add the query parameter to the updatedSrcUrl variable
     const updatedSrcUrl = `${srcUrl.src}&loggedIn=${loggedIn}&rehydrate=${rehydrate}&verifier=${verifier}&selectedAddress=${selectedAddress}&state=${state}`;
 
     // Update the src attribute of the iframe with the new URL
     window.parent.document.getElementById("upbond-iframe").src = updatedSrcUrl;
-    localStorage.setItem("upbond_widget", { selectedAddress, rehydrate, verifier, state, loggedIn });
+    localStorage.setItem("upbond_widget", JSON.stringify({ selectedAddress, rehydrate, verifier, state, loggedIn }));
     window.history.replaceState(null, "", window.location.origin + window.location.pathname);
   }
 }
@@ -28,5 +38,8 @@ window.addEventListener("message", function (event) {
   }
   if (event.data.type === "UPBOND_SELECTED_ADDRESS") {
     localStorage.setItem("upbond.selectedAddress", event.data.value);
+  }
+  if (event.data.type === "UPBOND_LOGOUT") {
+    localStorage.setItem("upbond_widget", event.data.value);
   }
 });
